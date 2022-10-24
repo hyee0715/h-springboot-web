35522309b7c10c62e2da21438b49418398be4bb7
hyee0715 <hyee0715@naver.com>
2022-10-24 17:50:02 +0900

    Initial commit

---
+++ src/main/java/practice/hspringbootweb/HSpringbootWebApplication.java
@@ -0,0 +1,13 @@
+package practice.hspringbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HSpringbootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HSpringbootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/hspringbootweb/HSpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.hspringbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HSpringbootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
