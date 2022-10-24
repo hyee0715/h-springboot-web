96f422f02df06df51a18c665d251b9462dda5767
hyee0715 <hyee0715@naver.com>
2022-10-24 18:18:56 +0900

    Spring Data JPA 테스트 코드 작성

---
+++ src/main/java/practice/hspringbootweb/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package practice.hspringbootweb.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
